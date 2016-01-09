39d8d9bbfa2c4bebd388a3a903052fd3862e1f47
Patryk Scheffler <patryk.scheffler@gmail.com>
2016-01-10 00:36:47 +0100

    Code refactor, cleanup

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -6,16 +6,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Generator {
-    public static int randomWithRange(int min, int max) {
+    /** Field that enables randomization */
+    private final static Random random = new Random();
+
+    /** Generate random integer in range */
+    public static int randomInRange(int min, int max) {
         int range = Math.abs(max - min) + 1;
-        return (int)(Math.random() * range) + (min <= max ? min : max);
+        return random.nextInt(range) + (min <= max ? min : max);
     }
-    
+
+    /** Chceck if break can be generated in that place */
     public static boolean checkBreak(int arr[][], int machine, int start, int time) {
-    	for(int i=start; i<=start+time; i++) {
-    		if(arr[machine][i] == 1) { return false; }
+    	if (arr[machine].length <= (start + time)) return false;
+        for (int i = start; i <= (start + time); i++) {
+    		if (arr[machine][i] == 1) { return false; }
     	}
     	return true;
     }
@@ -23,69 +30,73 @@ public class Generator {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Podaj numer instancji problemu do wygenerowania: ");
-        int instance = Integer.parseInt(br.readLine());
+        int instanceNumber = Integer.parseInt(br.readLine());
         System.out.print("Podaj ilosc zadan do wygenerowania w tej instancji: ");
-        int tasks = Integer.parseInt(br.readLine());
+        int tasksAmount = Integer.parseInt(br.readLine());
         System.out.print("Podaj minimalny czas wykonywania pojedynczej operacji: ");
-        int min_time = Integer.parseInt(br.readLine());
+        int minTaskTime = Integer.parseInt(br.readLine());
         System.out.print("Podaj maksymalny czas wykonywania pojedynczej operacji: ");
-        int max_time = Integer.parseInt(br.readLine());
+        int maxTaskTime = Integer.parseInt(br.readLine());
         System.out.print("Podaj minimalny czas trwania pojedynczej przerwy: ");
-        int min_break = Integer.parseInt(br.readLine());
+        int minBreakTime = Integer.parseInt(br.readLine());
         System.out.print("Podaj maksymalny czas trwania pojedynczej przerwy: ");
-        int max_break = Integer.parseInt(br.readLine());
-        int break_countM1 = randomWithRange(2, tasks/2);
-        int break_countM2 = randomWithRange(2, tasks/2);
-        int max_long = (max_time*2*tasks) + (int)Math.ceil(max_time*0.2*break_countM1) + (int)Math.ceil(max_time*0.2*break_countM2) + (break_countM1*max_break) + (break_countM2*max_break);
-        int[][] visual = new int[2][max_long+1];
-        for(int m=0; m<=1; m++) {
-        	Arrays.fill(visual[m], 0);
+        int maxBreakTime = Integer.parseInt(br.readLine());
+
+        int breakCountM1 = randomInRange(2, tasksAmount/2);
+        int breakCountM2 = randomInRange(2, tasksAmount/2);
+        int max_long = (maxTaskTime * 2 * tasksAmount) + (int) Math.ceil(maxTaskTime * 0.2 * breakCountM1) + (int) Math.ceil(maxTaskTime * 0.2 * breakCountM2) + (breakCountM1 * maxBreakTime) + (breakCountM2 * maxBreakTime);
+
+        int[][] visual = new int[2][max_long + 1];
+        for (int i = 0; i <= 1; i++) {
+        	Arrays.fill(visual[i], 0);
         }
-        visual[0][0] = break_countM1;
-        visual[1][0] = break_countM2;
-        String plik = "INSTANCJE/instancja" + instance + ".problem";
+        visual[0][0] = breakCountM1;
+        visual[1][0] = breakCountM2;
+
+        String plik = "INSTANCJE/instancja" + instanceNumber + ".problem";
         BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(plik));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
-        pw.write("**** " + Integer.toString(instance) + " ****");
-        pw.println();
-        pw.write(Integer.toString(tasks));
-        pw.println();
-        int t1;
-        int t2;
-        for(int i=1; i<=tasks; i++) {
-            t1 = randomWithRange(min_time, max_time);
-            t2 = randomWithRange(min_time, max_time);
-            pw.write(t1 + "; " + t2 + "; 1; 2;");
-            pw.println();
+        pw.write("**** " + Integer.toString(instanceNumber) + " ****\n");
+        pw.write(Integer.toString(tasksAmount) + "\n");
+
+        int op1Time;
+        int op2Time;
+        for (int i = 1; i <= tasksAmount; i++) {
+            op1Time = randomInRange(minTaskTime, maxTaskTime);
+            op2Time = randomInRange(minTaskTime, maxTaskTime);
+            pw.write(op1Time + "; " + op2Time + "; 1; 2;\n");
         }
-        int t;
-    	int start;
+
+        int breakTime;
+    	int startTime;
     	int counter = 1;
-    	for(int m=0; m<=1; m++) {
-	        for(int i=1; i<=visual[m][0]; i++) {
+    	for (int i = 0; i <= 1; i++) {
+	        for (int j = 1; j <= visual[i][0]; j++) {
 	        	do {
-	        		t = randomWithRange(min_break, max_break);
-	        		start = randomWithRange(0, max_long);
-	        	} while(!checkBreak(visual, m, start, t));
-	            pw.write(counter + "; " + m + "; " + t + "; " + start);
+	        		breakTime = randomInRange(minBreakTime, maxBreakTime);
+	        		startTime = randomInRange(0, max_long);
+	        	} while (!checkBreak(visual, i, startTime, breakTime));
+
+	            pw.write(counter + "; " + i + "; " + breakTime + "; " + startTime + "\n");
 	            counter++;
-	            pw.println();
-	            for(int j=start; j<=start+t; j++) {
-	            	visual[m][j] = 1;
+
+	            for (int k = startTime; k <= (startTime + breakTime); k++) {
+	            	visual[i][k] = 1;
 	            }
 	        }
     	}
+
         pw.write("*** EOF ***");
-        //WIZUALIZACJA:
-        /*pw.println();
-        for(int m=0; m<=1; m++) {
-	        for(int i=1; i<=max_long; i++) {
-	        	pw.write(Integer.toString(visual[m][i]));
-	        }
-	        pw.println();
-        }*/
-        //WIZUALIZACJA
+//        Wizualizacja przerw na maszynach:
+        pw.println();
+        for (int i = 0; i <= 1; i++) {
+            for(int j = 1; j <= max_long; j++) {
+                pw.write(Integer.toString(visual[i][j]));
+            }
+            pw.println();
+        }
+
         pw.close();
-        System.out.println("Zakonczono generowanie instancji problemu nr " + instance + " z " + tasks + " zadaniami!");
+        System.out.println("Zakonczono generowanie instancji problemu nr " + instanceNumber + " z " + tasksAmount + " zadaniami!");
     }
 }
