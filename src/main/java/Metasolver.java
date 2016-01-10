a94e7871f6a7cfe8668980027b8f67706558ea37
Patryk Scheffler <patryk.scheffler@gmail.com>
2016-01-10 18:43:47 +0100

    Added population generator, machine generator and break checker.

--- src/main/java/Metasolver.java
+++ src/main/java/Metasolver.java
@@ -3,8 +3,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /* TODO:
  * wymyslic jak przechowywac zadania i przerwy na maszynach
@@ -17,6 +18,7 @@ import java.util.List;
 public class Metasolver {
     private static List<Task> tasksContainer;
     private static List<Break> breaksContainer;
+    private static int tasksAmount;
 
     public static void loadInstance() throws IOException {
         tasksContainer = new ArrayList<>();
@@ -29,8 +31,7 @@ public class Metasolver {
         BufferedInputStream is = new BufferedInputStream(new FileInputStream(plik));
         BufferedReader read = new BufferedReader(new InputStreamReader(is));
         read.readLine();
-
-        int tasksAmount = Integer.parseInt(read.readLine());
+        tasksAmount = Integer.parseInt(read.readLine());
         String line;
         String taskParts[];
         int id = 1;
@@ -61,8 +62,121 @@ public class Metasolver {
 
     }
 
+    private static void randomPopulation(int amount) {
+        List<Integer> list = IntStream.range(1, tasksAmount + 1).boxed().collect(Collectors.toList());
+        List<Integer[]> population = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            Collections.shuffle(list);
+            population.add((Integer[]) list.toArray(new Integer[list.size()]));
+        }
+
+        for (int i = 0; i < population.size(); i++) {
+            for (int j = 0; j < population.get(i).length; j++) {
+                System.out.printf(String.valueOf(population.get(i)[j]));
+            }
+            System.out.printf("\n");
+            generateMachine(population.get(i));
+        }
+        System.out.printf("\n");
+    }
+
+    private static void generateMachine(Integer[] tasks) {
+        List<Integer[]> machine1 = new ArrayList<>();
+        List<Integer[]> machine2 = new ArrayList<>();
+        int maxTimeM1 = 0, maxTimeM2 = 0;
+
+        for (int i = 0; i < tasksAmount; i++) {
+            int op1Time = tasksContainer.get(tasks[i] - 1).getOp1().getTime();
+            int op2Time = tasksContainer.get(tasks[i] - 1).getOp2().getTime();
+            Break tmpBreak;
+
+            while ((tmpBreak = checkBreak(1, maxTimeM1, 1)) != null) {
+                int breakTime = tmpBreak.getTime();
+                machine1.add(new Integer[]{0, breakTime});
+                maxTimeM1 += breakTime;
+            }
+
+            do {
+                if ((tmpBreak = checkBreak(1, maxTimeM1, op1Time)) == null) {
+                    machine1.add(new Integer[]{tasks[i], op1Time});
+                    maxTimeM1 += op1Time;
+                    op1Time = 0;
+                } else {
+                    op1Time = (int) Math.ceil(op1Time * 1.2);
+                    int timeToBreak = tmpBreak.getStart() - maxTimeM1;
+                    machine1.add(new Integer[]{tasks[i], timeToBreak});
+                    maxTimeM1 += timeToBreak;
+                    op1Time -= timeToBreak;
+
+                    int breakTime = tmpBreak.getTime();
+                    machine1.add(new Integer[]{0, breakTime});
+                    maxTimeM1 += breakTime;
+                }
+            } while (op1Time > 0);
+
+            if (i == 0) {
+                machine2.add(new Integer[]{0, maxTimeM1});
+                maxTimeM2 = maxTimeM1;
+            }
+
+            while ((tmpBreak = checkBreak(2, maxTimeM2, 1)) != null) {
+                int breakTime = tmpBreak.getTime();
+                machine2.add(new Integer[]{0, breakTime});
+                maxTimeM2 += breakTime;
+            }
+
+            do {
+                if ((tmpBreak = checkBreak(2, maxTimeM2, op2Time)) == null) {
+                    machine2.add(new Integer[]{tasks[i], op2Time});
+                    maxTimeM2 += op2Time;
+                    op2Time = 0;
+                } else {
+                    op2Time = (int) Math.ceil(op2Time * 1.2);
+                    int timeToBreak = tmpBreak.getStart() - maxTimeM2;
+                    machine2.add(new Integer[]{tasks[i], timeToBreak});
+                    maxTimeM2 += timeToBreak;
+                    op2Time -= timeToBreak;
+
+                    int breakTime = tmpBreak.getTime();
+                    machine2.add(new Integer[]{0, breakTime});
+                    maxTimeM2 += breakTime;
+                }
+            } while (op2Time > 0);
+        }
+
+        for (int i = 0; i < machine1.size(); i++) {
+            for (int j = 0; j < machine1.get(i)[1]; j++) {
+                System.out.printf(String.valueOf(machine1.get(i)[0]));
+            }
+        }
+        System.out.printf("\n");
+        for (int i = 0; i < machine2.size(); i++) {
+            for (int j = 0; j < machine2.get(i)[1]; j++) {
+                System.out.printf(String.valueOf(machine2.get(i)[0]));
+            }
+        }
+        System.out.printf("\n");
+    }
+
+    private static Break checkBreak(int machine, int start, int time) {
+        for (int i = 0; i < breaksContainer.size(); i++) {
+            Break tmpBreak = breaksContainer.get(i);
+            if (tmpBreak.getMachine() != machine) continue;
+
+            int startBreak = tmpBreak.getStart();
+            int endBreak = startBreak + tmpBreak.getTime();
+            if ((start >= startBreak && start < endBreak) ||
+                    (startBreak >= start && startBreak < start + time)) {
+                return tmpBreak;
+            }
+        }
+        return null;
+    }
+
     public static void main(String args[]) throws NumberFormatException, IOException {
     	loadInstance();
+        randomPopulation(5);
 
         for (int i = 0; i < tasksContainer.size(); i++) {
             System.out.println(tasksContainer.get(i).getId() + ") " + tasksContainer.get(i).getOp1().getTime() + ", " + tasksContainer.get(i).getOp2().getTime());
